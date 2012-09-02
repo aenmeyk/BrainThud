0fcd69830aa04ed1a5f1c933cad54ca49a0c14d8
Kevin Aenmey <kevpublic@gmail.com>
2012-09-02 18:24:09 -0400

    Fixed When_Nugget_is_added_with_existing_keys to use correct keys

--- BrainThudTest/BrainThud.DataTest/AzureTableStorageTest/TableStorageRepositoryTest/When_Nugget_is_added_with_existing_keys.cs
+++ BrainThudTest/BrainThud.DataTest/AzureTableStorageTest/TableStorageRepositoryTest/When_Nugget_is_added_with_existing_keys.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using BrainThud.Model;
-using NUnit.Framework;
+﻿using BrainThud.Model;
 using FluentAssertions;
-using BrainThud.Data;
+using NUnit.Framework;
 
 namespace BrainThudTest.BrainThud.DataTest.AzureTableStorageTest.TableStorageRepositoryTest
 {
     [TestFixture]
     public class When_Nugget_is_added_with_existing_keys : Given_a_new_TableStorageRepository_of_Nugget
     {
-        private const string PARTITION_KEY = "ed518ec1-5133-4ed7-8ced-5636e1c12d2a";
-        private const string ROW_KEY = "746fecff-fcbf-4da0-b8c1-cd08fd06b1e8";
         private readonly Nugget nugget = new Nugget { PartitionKey = PARTITION_KEY, RowKey = ROW_KEY };
 
         public override void When()
