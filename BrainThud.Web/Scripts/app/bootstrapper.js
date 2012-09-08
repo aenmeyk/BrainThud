e32b208bec5770ef898e0065d7ae24225ac148f6
Kevin Aenmey <kevpublic@gmail.com>
2012-09-08 10:03:36 -0400

    Created UI bootstrapper

---
+++ BrainThud.Web/Scripts/app/bootstrapper.js
@@ -0,0 +1,16 @@
+﻿function ($, dataprimer, binder, routeConfig, presenter) {
+    var run = function () {
+        presenter.toggleActivity(true);
+
+        $.when(dataprimer.fetch())
+            .done(binder.bind)
+            .done(routeConfig.register)
+            .always(function () {
+                presenter.toggleActivity(false);
+            });
+    };
+
+    return {
+        run: run
+    };
+}
