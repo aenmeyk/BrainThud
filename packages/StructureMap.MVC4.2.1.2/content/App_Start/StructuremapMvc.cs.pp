14ec333bc7f47701504fe45fadd35ea187710a43
Kevin Aenmey <kevpublic@gmail.com>
2012-09-24 19:54:04 -0400

    Add Newtonsoft

---
+++ packages/StructureMap.MVC4.2.1.2/content/App_Start/StructuremapMvc.cs.pp
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using StructureMap;
+using $rootnamespace$.DependencyResolution;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.StructuremapMvc), "Start")]
+
+namespace $rootnamespace$.App_Start {
+    public static class StructuremapMvc {
+        public static void Start() {
+			IContainer container = IoC.Initialize();
+            DependencyResolver.SetResolver(new StructureMapDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new StructureMapDependencyResolver(container);
+        }
+    }
+}
