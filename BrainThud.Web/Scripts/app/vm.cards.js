9eb28d0b7d84dd19d9217bccf68cd1e074ad5e7a
Kevin Aenmey <kevpublic@gmail.com>
2012-11-06 21:06:52 -0500

    Added the ability to flip a card

--- BrainThud.Web/Scripts/app/vm.cards.js
+++ BrainThud.Web/Scripts/app/vm.cards.js
@@ -4,7 +4,8 @@
             cards = ko.observableArray(),
             currentCard = {
                 question: ko.observable(''),
-                answer: ko.observable('')
+                answer: ko.observable(''),
+                text: ko.observable('')
             },
         
             dataOptions = function () {
@@ -23,15 +24,29 @@
                     if(cards()[i].rowKey() === rowKey) {
                         currentCard.question(cards()[i].question());
                         currentCard.answer(cards()[i].answer());
+                        currentCard.text(cards()[i].question());
                         return;
                     }
+                }
+            },
+
+            flipCard = function () {
+                if(currentCard.text() == currentCard.question()) {
+                    currentCard.text(currentCard.answer());
+                    $('#card').removeClass('btn-warning');
+                    $('#card').addClass('btn-info');
+                } else {
+                    currentCard.text(currentCard.question());
+                    $('#card').removeClass('btn-info');
+                    $('#card').addClass('btn-warning');
                 }
             };
 
         return {
             cards: cards,
             currentCard: currentCard,
-            activate: activate
+            activate: activate,
+            flipCard: flipCard
         };
     }
 );
