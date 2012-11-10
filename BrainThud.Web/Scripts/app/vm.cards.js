ed0cee661c17fdebbb18f5fb5fb32721aba10ffa
Kevin Aenmey <kevpublic@gmail.com>
2012-11-09 22:04:11 -0500

    Added ability to navigate through cards

--- BrainThud.Web/Scripts/app/vm.cards.js
+++ BrainThud.Web/Scripts/app/vm.cards.js
@@ -1,6 +1,7 @@
 ﻿define('vm.cards', ['jquery', 'ko', 'data-context'],
     function ($, ko, dataContext) {
         var
+            nextCard = ko.observable(),
             cards = ko.observableArray(),
             currentCard = {
                 question: ko.observable(''),
@@ -25,26 +26,44 @@
                         currentCard.question(cards()[i].question());
                         currentCard.answer(cards()[i].answer());
                         currentCard.text(cards()[i].question());
+
+                        var nextCardIndex = i + 1;
+                        if (nextCardIndex >= cards().length - 1) {
+                            nextCardIndex = 0;
+                        }
+
+                        nextCard('#/quiz/' + cards()[nextCardIndex].rowKey());
+                        showQuestion();
                         return;
                     }
                 }
             },
-
-            flipCard = function () {
-                if(currentCard.text() == currentCard.question()) {
-                    currentCard.text(currentCard.answer());
-                    $('#card').removeClass('btn-warning');
-                    $('#card').addClass('btn-info');
-                } else {
+            
+            showQuestion = function () {
                     currentCard.text(currentCard.question());
                     $('#card').removeClass('btn-info');
                     $('#card').addClass('btn-warning');
+                },
+            
+            showAnswer = function () {
+                    currentCard.text(currentCard.answer());
+                    $('#card').removeClass('btn-warning');
+                    $('#card').addClass('btn-info');
+                },
+
+
+            flipCard = function () {
+                if(currentCard.text() == currentCard.question()) {
+                    showAnswer();
+                } else {
+                    showQuestion();
                 }
             };
 
         return {
             cards: cards,
             currentCard: currentCard,
+            nextCard: nextCard,
             activate: activate,
             flipCard: flipCard
         };
