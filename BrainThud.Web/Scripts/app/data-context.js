290d9b024eccda37d1dc3aaa324d2412069a79fd
Kevin Aenmey <kevpublic@gmail.com>
2012-11-16 14:39:31 -0500

    Created config literal for EntitySet

--- BrainThud.Web/Scripts/app/data-context.js
+++ BrainThud.Web/Scripts/app/data-context.js
@@ -1,64 +1,72 @@
 ﻿define('data-context', ['jquery', 'data-service', 'presenter', 'utils', 'model', 'model.mapper'],
     function ($, dataService, presenter, utils, model, modelMapper) {
-        var
-            EntitySet = function (getFunction, saveFunction) {
-                var
-                    items = [],
+        var EntitySet = function(config) {
+            var items = [],
+                getData = function(options) {
+                    var def = new $.Deferred();
 
-                    getData = function (options) {
-                        var def = new $.Deferred();
-
-                        var results = options && options.results;
-                        if (!items || !utils.hasProperties(items)) {
-                            getFunction({
-                                success: function (dtoList) {
-                                    for (var i = 0; i < dtoList.length; i++) {
-                                        items.push(modelMapper.card.fromDto(dtoList[i]));
-                                    }
-
-                                    if (results) {
-                                        results(items);
-                                    }
-                                    def.resolve(results);
-                                },
-                                error: function (response) {
-                                    if (def.reject) def.reject();
-                                    alert(response.statusText);
+                    var results = options && options.results;
+                    if (!items || !utils.hasProperties(items)) {
+                        config.get({
+                            success: function(dtoList) {
+                                for (var i = 0; i < dtoList.length; i++) {
+                                    items.push(config.mapper.fromDto(dtoList[i]));
                                 }
-                            });
-                        } else {
-                            results(items);
-                            def.resolve(results);
-                        }
-                        
-                        return def.promise();
-                    };
 
-                var saveData = function (options) {
-                    return $.Deferred(function (def) {
-                        saveFunction(options.data, {
-                            success: function (result) {
-                                presenter.showSuccess();
-                                def.resolve();
-                                options.createNewCard();
+                                if (results) {
+                                    results(items);
+                                }
+
+                                def.resolve(results);
                             },
-                            error: function (response) {
+                            error: function(response) {
                                 if (def.reject) def.reject();
                                 alert(response.statusText);
                             }
                         });
+                    } else {
+                        results(items);
+                        def.resolve(results);
+                    }
+
+                    return def.promise();
+                };
+
+            var saveData = function(options) {
+                return $.Deferred(function(def) {
+                    config.save(options.data, {
+                        success: function(result) {
+                            presenter.showSuccess();
+                            def.resolve();
+                            options.createNewCard();
+                        },
+                        error: function(response) {
+                            if (def.reject) def.reject();
+                            alert(response.statusText);
+                        }
                     });
-                };
+                });
+            };
 
-                return {
-                    getData: getData,
-                    saveData: saveData
-                };
-            },
+            return {
+                getData: getData,
+                saveData: saveData
+            };
+        };
 
-            cards = new EntitySet(dataService.cards.get, function () { });
-            card = new EntitySet(function () { }, dataService.cards.save);
-            quiz = new EntitySet(dataService.quiz.get, function () { });
+        cards = new EntitySet({
+            get: dataService.cards.get,
+            mapper: modelMapper.card
+        });
+        
+        card = new EntitySet({
+             save: dataService.cards.save
+        });
+        
+        quiz = new EntitySet({
+             get: dataService.quiz.get,
+             mapper: modelMapper.card
+        });
 
         return {
             cards: cards,
