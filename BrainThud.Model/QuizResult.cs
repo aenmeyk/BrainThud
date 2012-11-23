f4674a8d8a72a978f2b19e8180941a774e6b351f
Kevin Aenmey <kevpublic@gmail.com>
2012-11-23 17:10:37 -0500

    Added ITestData to QuizResult

--- BrainThud.Model/QuizResult.cs
+++ BrainThud.Model/QuizResult.cs
@@ -3,10 +3,11 @@ using Microsoft.WindowsAzure.StorageClient;
 
 namespace BrainThud.Model
 {
-    public class QuizResult : TableServiceEntity
+    public class QuizResult : TableServiceEntity, ITestData
     {
         public DateTime QuizDate { get; set; }
         public string CardId { get; set; }
         public bool IsCorrect { get; set; }
+        public bool IsTestData { get; set; }
     }
 }
