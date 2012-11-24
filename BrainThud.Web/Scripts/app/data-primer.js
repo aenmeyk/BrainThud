388de3c9888577a9c7a44ccc883bd973246b1859
Kevin Aenmey <kevpublic@gmail.com>
2012-11-24 13:37:08 -0500

    Prime quiz data

--- BrainThud.Web/Scripts/app/data-primer.js
+++ BrainThud.Web/Scripts/app/data-primer.js
@@ -1,14 +1,41 @@
-﻿define('data-primer', ['jquery', 'ko', 'data-context'],
-    function ($, ko, dataContext) {
+﻿define('data-primer', ['jquery', 'ko', 'data-context', 'vm'],
+    function ($, ko, dataContext, vm) {
         var
+            datePath,
+            quiz = ko.observableArray([]),
+            dataOptions = function () {
+                var today = new Date(),
+                year = today.getFullYear(),
+                month = today.getMonth() + 1,
+                day = today.getDate();
+
+                datePath = year + '/' + month + '/' + day;
+
+                return {
+                    results: quiz,
+                    params: {
+                        datePath: datePath
+                    }
+                };
+            },
+
             fetch = function () {
-//                return $.Deferred(function (def) {
-//                    //                    $.when(dataContext.quiz.getData()
-//                    //                        .fail(function () { def.reject(); }))
-//                    //                        .done(function () { def.resolve(); });
+                return $.Deferred(function (def) {
+                    $.when(dataContext.quiz.getData(dataOptions())
+                        .fail(function () { def.reject(); }))
+                        .done(function() {
+                            def.resolve();
+                            setQuizMenuUri(quiz()[0].cards[0].rowKey());
+                        });
 //                    def.resolve();
-//                }).promise();
-            };
+                }).promise();
+            },
+            
+
+        setQuizMenuUri = function(rowKey) {
+            $('#quizMenu').attr('href', '#/quizzes/' + datePath + '/' + rowKey);
+        };
+
 
         return {
             fetch: fetch
