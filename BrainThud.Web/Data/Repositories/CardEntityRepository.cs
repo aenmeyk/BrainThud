5bb07dbaa0211d5bb3594ea13809cf7f1a1fb9dd
Kevin Aenmey <kevpublic@gmail.com>
2012-12-08 15:45:45 -0500

    Added CardEntityRepository

---
+++ BrainThud.Web/Data/Repositories/CardEntityRepository.cs
@@ -0,0 +1,44 @@
+﻿using BrainThud.Web.Data.AzureTableStorage;
+using BrainThud.Web.Model;
+
+namespace BrainThud.Web.Data.Repositories
+{
+    public abstract class CardEntityRepository : TableStorageRepository<Card>, ICardRepository
+    {
+        private readonly string rowKeyPrefix;
+        protected string NameIdentifier { get; private set; }
+
+        protected CardEntityRepository(ITableStorageContext tableStorageContext, string nameIdentifier, string rowKeyPrefix) 
+            : base(tableStorageContext)
+        {
+            this.rowKeyPrefix = rowKeyPrefix;
+            this.NameIdentifier = nameIdentifier;
+        }
+
+        public Card GetCard(int userId, int cardId)
+        {
+            var partitionKey = this.GetPartitionKey(userId);
+            var rowKey = GetRowKey(cardId);
+
+            return this.Get(partitionKey, rowKey);
+        }
+
+        public void DeleteCard(int userId, int cardId)
+        {
+            var partitionKey = this.GetPartitionKey(userId);
+            var rowKey = GetRowKey(cardId);
+
+            this.Delete(partitionKey, rowKey);
+        }
+
+        private string GetRowKey(int cardId)
+        {
+            return string.Format("{0}-{1}", this.rowKeyPrefix, cardId);
+        }
+
+        private string GetPartitionKey(int userId)
+        {
+            return string.Format("{0}-{1}", this.NameIdentifier, userId);
+        }
+    }
+}
--- BrainThud.Web/Data/Repositories/CardRepository.cs
+++ BrainThud.Web/Data/Repositories/CardRepository.cs
@@ -1,42 +1,10 @@
 ﻿using BrainThud.Web.Data.AzureTableStorage;
-using BrainThud.Web.Model;
 
 namespace BrainThud.Web.Data.Repositories
 {
-    public class CardRepository : TableStorageRepository<Card>, ICardRepository
+    public class CardRepository : CardEntityRepository
     {
-        private readonly string nameIdentifier;
-
         public CardRepository(ITableStorageContext tableStorageContext, string nameIdentifier) 
-            : base(tableStorageContext)
-        {
-            this.nameIdentifier = nameIdentifier;
-        }
-
-        public Card GetCard(int userId, int cardId)
-        {
-            var partitionKey = this.GetPartitionKey(userId);
-            var rowKey = GetRowKey(cardId);
-
-            return this.Get(partitionKey, rowKey);
-        }
-
-        public void DeleteCard(int userId, int cardId)
-        {
-            var partitionKey = this.GetPartitionKey(userId);
-            var rowKey = GetRowKey(cardId);
-
-            this.Delete(partitionKey, rowKey);
-        }
-
-        private static string GetRowKey(int cardId)
-        {
-            return string.Format("{0}-{1}", CardRowTypes.CARD, cardId);
-        }
-
-        private string GetPartitionKey(int userId)
-        {
-            return string.Format("{0}-{1}", this.nameIdentifier, userId);
-        }
+            : base(tableStorageContext, nameIdentifier, CardRowTypes.CARD) {}
     }
 }
