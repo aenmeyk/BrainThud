45afac43e2646b00f66bd64268550e0feeb9914a
Kevin Aenmey <kevpublic@gmail.com>
2012-12-15 15:31:49 -0500

    Prevent IIS from going to sleep

---
+++ BrainThud.Web/DisableTimeout.cmd
@@ -0,0 +1,4 @@
+﻿%windir%\system32\inetsrv\appcmd set config -section:applicationPools -applicationPoolDefaults.processModel.loadUserProfile:true
+%windir%\system32\inetsrv\appcmd set config -section:applicationPools -applicationPoolDefaults.processModel.idleTimeout:00:00:00
+%windir%\system32\inetsrv\appcmd set config -section:applicationPools -applicationPoolDefaults.recycling.periodicRestart.time:96:00:00
+exit /b 0
