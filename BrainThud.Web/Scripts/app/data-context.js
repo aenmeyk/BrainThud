2749e82ee4ab0ba171f44be9d80a902f487fb682
Kevin Aenmey <kevpublic@gmail.com>
2012-12-17 21:21:54 -0500

    Created first data-context test

--- BrainThud.Web/Scripts/app/data-context.js
+++ BrainThud.Web/Scripts/app/data-context.js
@@ -1,73 +1,70 @@
 ﻿define('data-context', ['jquery', 'data-service', 'utils', 'model', 'model.mapper'],
     function ($, dataService, utils, model, modelMapper) {
-        var EntitySet = function (config) {
-            var cachedResults = [],
-                getData = function (options) {
-                    var def = new $.Deferred();
-
-                    var results = options && options.results;
-                    if (!cachedResults || !utils.hasProperties(cachedResults)) {
-                        config.get({
-                            params: options.params,
-                            success: function (dto) {
-                                cachedResults = [];
-
-                                config.mapper.mapResults(dto, cachedResults);
-
-                                if (results) {
-                                    results(cachedResults);
-                                }
-
-                                def.resolve(results);
-                            },
-                            error: function () {
-                                if (def.reject) def.reject();
-                            }
-                        });
-                    } else {
-                        results(cachedResults);
-                        def.resolve(results);
-                    }
-
-                    return def.promise();
-                },
-
-                createData = function (options) {
-                    return $.Deferred(function (def) {
-                        config.create(options.data, {
-                            params: options.params,
-                            success: function (result) {
-                                def.resolve();
-                            },
-                            error: function (response) {
-                                if (def.reject) def.reject();
-                            }
-                        });
-                    });
-                },
-
-                updateData = function (options) {
-                    return $.Deferred(function (def) {
-                        config.update(options.data, {
-                            params: options.params,
-                            success: function (result) {
-                                def.resolve();
-                            },
-                            error: function (response) {
-                                if (def.reject) def.reject();
-                            }
-                        });
-                    });
-                };
-
-            return {
-                getData: getData,
-                createData: createData,
-                updateData: updateData
-            };
-        };
-
         var
+            EntitySet = function (config) {
+                var cachedResults = [],
+                    getData = function (options) {
+                        var def = new $.Deferred();
+
+                        var results = options && options.results;
+                        if (!cachedResults || !utils.hasProperties(cachedResults)) {
+                            config.get({
+                                success: function (dto) {
+                                    cachedResults = [];
+
+                                    config.mapper.mapResults(dto, cachedResults);
+
+                                    if (results) {
+                                        results(cachedResults);
+                                    }
+
+                                    def.resolve(results);
+                                },
+                                error: function () {
+                                    if (def.reject) def.reject();
+                                }
+                            });
+                        } else {
+                            results(cachedResults);
+                            def.resolve(results);
+                        }
+
+                        return def.promise();
+                    },
+
+                    createData = function (options) {
+                        return $.Deferred(function (def) {
+                            config.create(options.data, {
+                                success: function (result) {
+                                    def.resolve();
+                                },
+                                error: function (response) {
+                                    if (def.reject) def.reject();
+                                }
+                            });
+                        });
+                    },
+
+                    updateData = function (options) {
+                        return $.Deferred(function (def) {
+                            config.update(options.data, {
+                                success: function (result) {
+                                    def.resolve();
+                                },
+                                error: function (response) {
+                                    if (def.reject) def.reject();
+                                }
+                            });
+                        });
+                    };
+
+                return {
+                    getData: getData,
+                    createData: createData,
+                    updateData: updateData
+                };
+            },
+
             cards = new EntitySet({
                 get: dataService.card.get,
                 mapper: modelMapper.cardHtml
---
+++ BrainThud.Web/Test/Scripts/data-context-test.js
@@ -0,0 +1,83 @@
+﻿/// <reference path="../../Scripts/qunit.js" />
+/// <reference path="test-setup.js" />
+/// <reference path="../../Scripts/app/data-context.js" />
+
+// Setup mocks
+var entitySetMock = {
+    get: function (callbacks) {
+        callbacks.success(dto);
+    },
+    create: function (data, callbacks) {
+        callbacks.success(dto);
+    },
+    update: function (data, callbacks) {
+        callbacks.success(dto);
+    },
+    mapper: {
+        mapResults: function (dto, cachedResults) {
+            
+        }
+    },
+};
+var
+    cashedResultsAreMappedToResults = false,
+    $ = {
+        Deferred: function () {
+            return {
+                promise: function () {
+
+                },
+                resolve: function (results) {
+
+                }
+            };
+        }
+    },
+
+    dataService = {
+        card: entitySetMock,
+        quiz: entitySetMock,
+        quizResult: entitySetMock
+    },
+
+    utils = {
+        hasProperties: function (results) {
+
+        }
+    },
+
+    model = {
+    },
+
+    modelMapper = {
+        cardHtml: {
+            mapResults: function (dto, cachedResults) {
+                cashedResultsAreMappedToResults = true;
+            }
+        }
+    },
+
+    options = {
+        results: function (cachedResults) {
+
+        }
+    },
+
+    dto = {
+
+    };
+
+// Tests
+(function () {
+    var result;
+
+    module('Given a data-context.cards entitSet, when getData is called', {
+        setup: function () {
+            result = itemToTest($, dataService, utils, model, modelMapper).cards.getData();
+        }
+    });
+
+    test('Then the cached results are mapped to the actual results', function () {
+        ok(cashedResultsAreMappedToResults);
+    });
+})();
