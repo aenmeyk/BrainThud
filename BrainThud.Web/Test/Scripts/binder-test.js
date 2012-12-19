28454af2a1f7939f3f06eb3977ac960b7298c5e4
Kevin Aenmey <kevpublic@gmail.com>
2012-12-19 16:12:34 -0500

    Removed qUnit tests

--- BrainThud.Web/Test/Scripts/binder-test.js
+++ BrainThud.Web/Test/Scripts/binder-test.js
@@ -1,60 +1,47 @@
-﻿/// <reference path="../../Scripts/qunit.js" />
-/// <reference path="test-setup.js" />
+﻿/// <reference path="../../Scripts/jasmine/jasmine.js" />
+/// <reference path="jasmine-test-setup.js" />
+/// <reference path="../../Scripts/jquery-1.8.2.js" />
+/// <reference path="../../Scripts/knockout-2.2.0.debug.js" />
 /// <reference path="../../Scripts/app/binder.js" />
 
-// Setup mocks
-var
-    $ = function (viewName) {
-        return {
-            get: function(value) {
-                return viewName;
+describe("A binder module, when bind() is called", function () {
+    var
+        vm = {
+            card: 'card ViewModel',
+            createCard: 'createCard ViewModel',
+            cards: 'cards ViewModel',
+            quiz: 'quiz ViewModel'
+        },
+        config = {
+            viewIds: {
+                card: 'card view ID',
+                createCard: 'createCard view ID',
+                cards: 'cards view ID',
+                quiz: 'quiz view ID'
             }
         };
-    },
-
-    ko = {
-        applyBindings: function (viewModel, rootNode) {
-            appliedBindings[viewModel] = rootNode;
-        }
-    },
-
-    appliedBindings = [],
-    vm = {
-        card: 'card ViewModel',
-        createCard: 'createCard ViewModel',
-        cards: 'cards ViewModel',
-        quiz: 'quiz ViewModel',
-    },
-    config = {
-        viewIds: {
-            card: 'card view ID',
-            createCard: 'createCard view ID',
-            cards: 'cards view ID',
-            quiz: 'quiz view ID',
-        }
-    };
-
-// Tests
-(function () {
-    module('Given a binder module, when bind() is called', {
-        setup: function () {
-            itemToTest($, ko, vm, config).bind();
-        }
+    
+    beforeEach(function () {
+        spyOn(jQuery.fn, "get").andCallFake(function() {
+            return this.selector;
+        });
+        spyOn(ko, "applyBindings");
+        modules['binder'](jQuery, ko, vm, config).bind();
     });
 
-    test('Then the create card view model should be bound to the create card view', function () {
-        equal(appliedBindings[vm.card], config.viewIds.card);
+    it('should bind the card view model to the card view', function () {
+        expect(ko.applyBindings).toHaveBeenCalledWith(vm.card, config.viewIds.card);
     });
 
-    test('Then the card view model should be bound to the card view', function () {
-        equal(appliedBindings[vm.createCard], config.viewIds.createCard);
+    it('should bind the create card view model to the create card view', function () {
+        expect(ko.applyBindings).toHaveBeenCalledWith(vm.createCard, config.viewIds.createCard);
     });
 
-    test('Then the cards view model should be bound to the cards view', function () {
-        equal(appliedBindings[vm.cards], config.viewIds.cards);
+    it('should bind the cards view model to the cards view', function () {
+        expect(ko.applyBindings).toHaveBeenCalledWith(vm.cards, config.viewIds.cards);
     });
 
-    test('Then the quiz view model should be bound to the quiz view', function () {
-        equal(appliedBindings[vm.quiz], config.viewIds.quiz);
+    it('should bind the quiz view model to the quiz view', function () {
+        expect(ko.applyBindings).toHaveBeenCalledWith(vm.quiz, config.viewIds.quiz);
     });
-})();
+});
