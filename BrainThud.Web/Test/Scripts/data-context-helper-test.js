44f6193bff15137d200a0c1bea1b69195071ecb1
Kevin Aenmey <kevpublic@gmail.com>
2012-12-20 22:23:19 -0500

    Creating data-context-helper tests

--- BrainThud.Web/Test/Scripts/data-context-helper-test.js
+++ BrainThud.Web/Test/Scripts/data-context-helper-test.js
@@ -1,26 +1,42 @@
 ﻿/// <reference path="../../Scripts/jasmine/jasmine.js" />
 /// <reference path="jasmine-test-setup.js" />
 /// <reference path="../../Scripts/jquery-1.8.2.js" />
-/// <reference path="../../Scripts/app/data-context.js" />
+/// <reference path="../../Scripts/app/data-context-helper.js" />
 
-describe("A data-context module, when cards is called", function () {
-    var dataService, utils, modelMapper, card;
+describe("Given a new data-context-helper.EntitySet", function () {
+    var dataContextHelper, utils, deferred, EntitySet,
+        promiseResult = 'jquery promise result';
 
     beforeEach(function () {
-
-        dataService = {
-            card: jasmine.createSpyObj('dataService.card', ['get', 'create', 'update']),
-            quiz: jasmine.createSpyObj('dataService.quiz', ['get']),
-            quizResult: jasmine.createSpyObj('dataService.quizResult', ['create'])
-        };
-
+        spyOn(jQuery, "Deferred").andCallFake(function() {
+            deferred = jasmine.createSpyObj('deferred', ['resolve', 'reject', 'promise']);
+            deferred.promise.andCallFake(function () {
+                return promiseResult;
+            });
+            return deferred;
+        });
         utils = jasmine.createSpyObj('utils', ['hasProperties']);
-        modelMapper = jasmine.createSpyObj('modelMapper', ['card', 'cardHtml', 'quiz']);
-
-        card = modules['data-context'](jQuery, dataService, utils, modelMapper).cards;
+        utils.hasProperties.andCallFake(function() {
+            return true;
+        });
+        dataContextHelper = modules['data-context-helper'];
+        EntitySet = new dataContextHelper(jQuery, utils).EntitySet();
     });
+    
+    describe("when a getData() is called with no cached results", function () {
+        var result,
+            optionsResults = function () {};
 
-    it('the card.get should be equal dataService.card.get', function () {
-        expect(card.get).toEqual(dataService.card.get);
+        beforeEach(function () {
+            result = EntitySet.getData({ results: optionsResults });
+        });
+
+        it('then the result of the jquery promise is returned', function () {
+            expect(result).toEqual(promiseResult);
+        });
+
+        it('then the jquery deferred.resolve should be resolved with the options.results', function () {
+            expect(deferred.resolve).toHaveBeenCalledWith(optionsResults);
+        });
     });
 });
