e2285f81588868d39186923dc4c8762f3148af44
Kevin Aenmey <kevpublic@gmail.com>
2013-01-07 18:06:10 -0500

    Implement Correct & Incorrect count in vm.quiz

--- BrainThud.Web/Scripts/app/vm.quiz.js
+++ BrainThud.Web/Scripts/app/vm.quiz.js
@@ -2,26 +2,26 @@
     function (ko, _, moment, quizNavigator, amplify, config, dataContext, utils, global) {
         var
             init = function() {
-                amplify.subscribe(config.pubs.quizResultCacheChanged, function (data) {
-
-                    var cardIds = _.map(data, function(item) {
-                        return item.cardId();
-                    });
-
-                    var results = quizResults();
-                    _.each(cardIds, function(cardId) {
-                        for (var i = 0; i < results.length; i++) {
-                            if (results[i].cardId == cardId) {
-                                quizResults.splice(i, 1);
-                                break;
-                            }
-                        }
-                    });
-
-                    _.each(data, function (item) {
-                        quizResults.push(ko.toJS(item));
-                    });
-                });
+//                amplify.subscribe(config.pubs.quizResultCacheChanged, function (data) {
+//
+//                    var cardIds = _.map(data, function(item) {
+//                        return item.cardId();
+//                    });
+//
+//                    var results = quizResults();
+//                    _.each(cardIds, function(cardId) {
+//                        for (var i = 0; i < results.length; i++) {
+//                            if (results[i].cardId == cardId) {
+//                                quizResults.splice(i, 1);
+//                                break;
+//                            }
+//                        }
+//                    });
+//
+//                    _.each(data, function (item) {
+//                        quizResults.push(ko.toJS(item));
+//                    });
+//                });
             },
 
             quizDate = moment().format('L'),
@@ -35,22 +35,29 @@
             completedCardCount = ko.computed(function () {
                 return quizResults().length;
             }),
-            
+
             correctCardCount = ko.computed(function () {
-                return countQuizResults(function(quizResult) {
-                    return quizResult.isCorrect;
-                });
+                return _.filter(quizResults(), function(quizResult) {
+                    return quizResult.isCorrect();
+                }).length;
             }),
-            
+
             incorrectCardCount = ko.computed(function () {
-                return countQuizResults(function (quizResult) {
-                    return !quizResult.isCorrect;
-                });
+                return _.filter(quizResults(), function (quizResult) {
+                    return !quizResult.isCorrect();
+                }).length;
             }),
 
             activate = function() {
                 dataContext.quizCards.getData({
                     results: cards,
+                    params: {
+                        datePath: utils.getDatePath(),
+                        userId: global.userId
+                    }
+                });
+                dataContext.quizResult.getData({
+                    results: quizResults,
                     params: {
                         datePath: utils.getDatePath(),
                         userId: global.userId
@@ -63,16 +70,6 @@
             };
         
         init();
-                    
-        function countQuizResults(shouldCount) {
-            var correct = 0;
-            var results = quizResults();
-            for (var i = 0; i < quizResults().length; i++) {
-                if (shouldCount(results[i])) correct++;
-            }
-            return correct;
-        };
-
 
         return {
             quizDate: quizDate,
