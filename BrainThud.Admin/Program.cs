52f2b1bae246cf7a4b6c91ce5e75da69bcf270c7
Kevin Aenmey <kevpublic@gmail.com>
2013-01-15 10:21:44 -0500

    Added BrainThud.Admin project to solution

---
+++ BrainThud.Admin/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Configuration;
+using BrainThud.Web;
+using BrainThud.Web.Data.AzureTableStorage;
+using BrainThud.Web.DependencyResolution;
+using Microsoft.WindowsAzure;
+
+namespace BrainThud.Admin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Initialize();
+
+            var container = IoC.Initialize();
+            var contextFactory = container.GetInstance<ITableStorageContextFactory>();
+            var context = contextFactory.CreateTableStorageContext(AzureTableNames.CARD);
+
+            var cards = context.Cards.GetAll();
+
+            foreach(var card in cards)
+            {
+                context.Cards.Update(card);
+            }
+
+            // context.Commit();
+        }
+
+        static void Initialize()
+        {
+            CloudStorageAccount.SetConfigurationSettingPublisher((configName, configSetter) =>
+            {
+                var connectionString = ConfigurationManager.AppSettings[configName];
+                configSetter(connectionString);
+            });
+        }
+    }
+}
