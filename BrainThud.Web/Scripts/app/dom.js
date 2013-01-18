0c8e82a4bd70f17a2b15662fb619df71416f556c
Kevin Aenmey <kevpublic@gmail.com>
2013-01-17 22:33:15 -0500

    Add glyph to mobile view card deck name

--- BrainThud.Web/Scripts/app/dom.js
+++ BrainThud.Web/Scripts/app/dom.js
@@ -17,6 +17,23 @@
                 return $('#wmd-input-question-create').length != 0;
             };
 
+        var $cardDeckChevron = $('#card-deck-chevron');
+        var $cardDeckContainer = $('.card-deck-container');
+
+        $('.card-deck-name').toggle(function () {
+            $cardDeckContainer.collapse('toggle');
+            setTimeout(function () {
+                $cardDeckChevron.addClass("icon-chevron-up");
+                $cardDeckChevron.removeClass("icon-chevron-down");
+            }, 100);
+        }, function () {
+            $cardDeckContainer.collapse('toggle');
+            setTimeout(function () {
+                $cardDeckChevron.addClass("icon-chevron-down");
+                $cardDeckChevron.removeClass("icon-chevron-up");
+            }, 100);
+        });
+
         return {
             getCardValues: getCardValues,
             resetNewCard: resetNewCard,
