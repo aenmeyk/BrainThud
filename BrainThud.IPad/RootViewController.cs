cfc842aebcf4b3a1801436556cb2976ecf536929
Kevin Aenmey <kevpublic@gmail.com>
2013-01-20 12:09:19 -0500

    Added IPad project

---
+++ BrainThud.IPad/RootViewController.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace BrainThud.IPad
+{
+	public partial class RootViewController : UITableViewController
+	{
+		public RootViewController () : base ("RootViewController", null)
+		{
+			this.ClearsSelectionOnViewWillAppear = false;
+			this.ContentSizeForViewInPopover = new SizeF (320f, 600f);
+			
+			// Custom initialization
+		}
+		
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+			
+			this.TableView.Source = new DataSource (this);
+			this.TableView.SelectRow (NSIndexPath.FromRowSection (0, 0), false, UITableViewScrollPosition.Middle);
+		}
+		
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		{
+			// Return true for supported orientations
+			return true;
+		}
+		
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+		
+		public override void ViewDidUnload ()
+		{
+			base.ViewDidUnload ();
+			
+			// Clear any references to subviews of the main view in order to
+			// allow the Garbage Collector to collect them sooner.
+			//
+			// e.g. myOutlet.Dispose (); myOutlet = null;
+			
+			ReleaseDesignerOutlets ();
+		}
+		
+		class DataSource : UITableViewSource
+		{
+			RootViewController controller;
+
+			public DataSource (RootViewController controller)
+			{
+				this.controller = controller;
+			}
+			
+			// Customize the number of sections in the table view.
+			public override int NumberOfSections (UITableView tableView)
+			{
+				return 1;
+			}
+			
+			public override int RowsInSection (UITableView tableview, int section)
+			{
+				return 1;
+			}
+			
+			// Customize the appearance of table view cells.
+			public override UITableViewCell GetCell (UITableView tableView, MonoTouch.Foundation.NSIndexPath indexPath)
+			{
+				string cellIdentifier = "Cell";
+				var cell = tableView.DequeueReusableCell (cellIdentifier);
+				if (cell == null) {
+					cell = new UITableViewCell (UITableViewCellStyle.Default, cellIdentifier);
+				}
+				
+				// Configure the cell.
+				cell.TextLabel.Text = NSBundle.MainBundle.LocalizedString ("Detail", "Detail");
+				return cell;
+			}
+
+			/*
+			// Override to support conditional editing of the table view.
+			public override bool CanEditRow (UITableView tableView, MonoTouch.Foundation.NSIndexPath indexPath)
+			{
+				// Return false if you do not want the specified item to be editable.
+				return true;
+			}
+			*/
+			
+			/*
+			// Override to support editing the table view.
+			public override void CommitEditingStyle (UITableView tableView, UITableViewCellEditingStyle editingStyle, NSIndexPath indexPath)
+			{
+				if (editingStyle == UITableViewCellEditingStyle.Delete) {
+					// Delete the row from the data source.
+					controller.TableView.DeleteRows (new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Fade);
+				} else if (editingStyle == UITableViewCellEditingStyle.Insert) {
+					// Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+				}
+			}
+			*/
+			
+			/*
+			// Override to support rearranging the table view.
+			public override void MoveRow (UITableView tableView, NSIndexPath sourceIndexPath, NSIndexPath destinationIndexPath)
+			{
+			}
+			*/
+			
+			/*
+			// Override to support conditional rearranging of the table view.
+			public override bool CanMoveRow (UITableView tableView, NSIndexPath indexPath)
+			{
+				// Return false if you do not want the item to be re-orderable.
+				return true;
+			}
+			*/
+			
+			public override void RowSelected (UITableView tableView, NSIndexPath indexPath)
+			{
+				// Navigation logic may go here -- for example, create and push another view controller.
+			}
+		}
+	}
+}
+
