10fb1d396fda3cc46129fe127f5c8b86f68eeb84
Kevin Aenmey <kevpublic@gmail.com>
2013-01-26 09:09:24 -0500

    Update default calendar intervals

--- BrainThud.Web/Calendars/DefaultQuizCalendar.cs
+++ BrainThud.Web/Calendars/DefaultQuizCalendar.cs
@@ -5,11 +5,11 @@
         public DefaultQuizCalendar()
         {
             this.Add(0, 1);
-            this.Add(1, 6);
-            this.Add(2, 24);
-            this.Add(3, 66);
-            this.Add(4, 114);
-            this.Add(5, 246);
+            this.Add(1, 5);
+            this.Add(2, 14);
+            this.Add(3, 28);
+            this.Add(4, 60);
+            this.Add(5, 120);
         }
     }
 }
