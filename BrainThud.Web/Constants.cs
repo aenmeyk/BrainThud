d17dbc316a83d985fc05698c16588bc063172d33
Kevin Aenmey <kevpublic@gmail.com>
2013-01-29 17:20:19 -0500

    Update # seed identities

--- BrainThud.Web/Constants.cs
+++ BrainThud.Web/Constants.cs
@@ -74,7 +74,7 @@ namespace BrainThud.Web
     {
         public const string AZURE_STORAGE = "DataConnectionString";
         public const int CONCURRENCY_VIOLATION_RETRIES = 10;
-        public const int SEED_IDENTITIES = 1000;
+        public const int SEED_IDENTITIES = 200;
         public const int IDENTITY_QUEUE_VISIBILITY_TIMEOUT_SECONDS = 10;
         public static int SeedRefreshIntervalSeconds = 120;
     }
