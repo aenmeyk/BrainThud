161501d37d347f8ddbaa5c0e7ce5d73268610748
Kevin Aenmey <kevpublic@gmail.com>
2013-01-31 19:03:44 -0500

    Added framework for card-manager

--- BrainThud.Web/Scripts/app/card-manager.js
+++ BrainThud.Web/Scripts/app/card-manager.js
@@ -1,55 +1,56 @@
-﻿define('card-manager', ['jquery', 'ko', 'router', 'data-context', 'utils', 'dom', 'amplify', 'config', 'global', 'vm'],
-    function ($, ko, router, dataContext, utils, dom, amplify, config, global, vm) {
+﻿define('card-manager', ['ko', 'data-context', 'global'],
+    function (ko, dataContext, global) {
         var
-            deleteCardOptions,
-            register = function () {
-                var $deleteDialog = $('#deleteDialog');
-                var $cardInfoDialog = $('#card-info-dialog');
-                $('body').on('click.modal.data-api', '[data-toggle="delete"]', function () {
-                    deleteCard();
-                });
-
-                amplify.subscribe(config.pubs.showEditCard, function (cardId) {
-                    router.navigateTo(global.routePrefix + 'cards/' + cardId + '/edit');
-                });
-
-                amplify.subscribe(config.pubs.showDeleteCard, function (data, callback) {
-                    deleteCardOptions = {
-                        currentCard: data,
-                        callback: callback
-                    };
-                    $deleteDialog.modal('show');
-                });
-
-                amplify.subscribe(config.pubs.showCardInfo, function (data) {
-                    vm.cardInfo.activate(data);
-                    $cardInfoDialog.modal('show');
+            cards = ko.observableArray([]),
+            quizCards = ko.observableArray([]),
+            
+            init = function() {
+                dataContext.card.getData({
+                    results: cards
                 });
             },
 
+            createCard = function () {
+
+            },
+
+            updateCard = function () {
+
+            },
+
             deleteCard = function () {
-                $("#deleteDialog").modal('hide');
-                $.when(dataContext.card.deleteData({
+
+            },
+
+            getQuizCards = function (year, month, day) {
+                dataContext.quizResult.getData({
+                    results: quizResults,
                     params: {
-                        userId: global.userId,
-                        partitionKey: deleteCardOptions.currentCard.partitionKey(),
-                        rowKey: deleteCardOptions.currentCard.rowKey(),
-                        entityId: deleteCardOptions.currentCard.entityId()
-                    }
-                })).then(function () {
-                    dataContext.quizCard.removeCachedItem(deleteCardOptions.currentCard);
-                    dataContext.quizResult.refreshCache();
-                    amplify.publish(config.pubs.cardDeleted, deleteCardOptions.currentCard.entityId());
-                    amplify.publish(config.pubs.deleteCard);
-                    if (deleteCardOptions.callback) {
-                        deleteCardOptions.callback();
+                        datePath: moment([year, month, day]).format('YYYY/M/D'),
+                        userId: global.userId
                     }
                 });
+            },
+
+            shuffleQuizCards = function () {
+
+            },
+
+            applyQuizResult = function () {
+
             };
 
+        init();
         
         return {
-            register: register
+            cards: cards,
+            quizCards: quizCards,
+            createCard: createCard,
+            updateCard: updateCard,
+            deleteCard: deleteCard,
+            getQuizCards: getQuizCards,
+            shuffleQuizCards: shuffleQuizCards,
+            applyQuizResult: applyQuizResult
         };
     }
 );
